6e4e69f0bb1695c92254c9cd122dd92b4bee55d9
Aram Gokce <aramgokce@gmail.com>
2026-07-01 22:00:29 -0400

    Initial Commit

---
+++ DiscAppline/DiscApplineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscApplineApp.swift
+//  DiscAppline
+//
+//  Created by Aram Gokce on 7/1/26.
+//
+
+import SwiftUI
+
+@main
+struct DiscApplineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
